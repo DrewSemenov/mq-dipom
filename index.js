cda81aec51b19a14d7076c1a3b5a2e248e1d58d2
Andrei Semenov <drew.semenow@gmail.com>
2023-07-01 21:21:44 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let mobileMenu = document.querySelector(".mobile-menu");
+let popupWrapper = document.querySelector(".popup-wrapper");
+let buttonClose = document.querySelector(".button-close");
+
+console.log(mobileMenu);
+
+mobileMenu.addEventListener("click", () => {
+  popupWrapper.classList.add("popup-active");
+  mobileMenu.classList.remove("menu-active");
+});
+
+buttonClose.addEventListener("click", () => {
+  popupWrapper.classList.remove("popup-active");
+  mobileMenu.classList.add("menu-active");
+});
